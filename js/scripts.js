0df237fdf36a9ec46dd21f7b3905d46d4e400d25
tweyrauch <tiayrock@gmail.com>
2014-05-18 16:54:45 -0700

    Version 17
    *styling of plugin
    *edited jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
 var folded = new OriDomi(document.getElementsByClassName('map')[0]);
 var folded = new OriDomi('.map', {
-  vPanels:         5,     // number of panels when folding left or right (vertically oriented)
-  speed:           1200,  // folding duration in ms
-  ripple:          2,     // backwards ripple effect when animating
-  shadingIntesity: .5,    // lessen the shading effect
-  perspective:     800,   // smaller values exaggerate 3D distortion
-  shading:         'soft' // change the shading type
+  vPanels:         [30,17,17,17,18],     // number of panels when folding left or right (vertically oriented)
+  shadingIntesity: .9,    // lessen the shading effect
+  perspective:     600,   // smaller values exaggerate 3D distortion
+  shading:         'hard' // change the shading type
 });
 
-folded.reveal(30);
+folded.reveal(50);
 
